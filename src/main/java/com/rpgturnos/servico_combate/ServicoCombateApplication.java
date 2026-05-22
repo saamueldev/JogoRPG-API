d0683fde3a431af398ccb276dccb36d41ed63f93
Samuel Rocha <saamueldev@gmail.com>
2026-05-22 13:46:07 -0300

    Arquivos

---
+++ src/main/java/com/rpgturnos/servico_combate/ServicoCombateApplication.java
@@ -0,0 +1,13 @@
+package com.rpgturnos.servico_combate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicoCombateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicoCombateApplication.class, args);
+	}
+
+}
